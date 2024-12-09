0d16f7a3e5e991981594b73dac4677d6977d2f17
Namanhack <147086418+naman0867@users.noreply.github.com>
2024-12-09 23:10:18 +0530

    Ecom

---
+++ allbooks/30dc all coding/1.java
@@ -0,0 +1,14 @@
+// Java Program to demonstrate int data-type
+
+class GFG 
+{
+    public static void main (String[] args) 
+    {
+          // declaring two int variables
+        int a = 10;
+          int b = 20;
+      
+          System.out.println( a + b );
+    }
+
+
